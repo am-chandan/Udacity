aaebc5663423ccf6540b9b6dd2e711c6c1512825
am-chandan <93567721+am-chandan@users.noreply.github.com>
2021-11-02 10:06:53 +0530

    Add files via upload

---
+++ udac_example_dag.py
@@ -0,0 +1,46 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator, LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catch': False,
+    'email_on_retry': False
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *',
+          max_active_runs=1
+        )
+
+#start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    target_table="staging_songs",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    copy_format="'auto'",
+    provide_context=True
+)
+
+
+#end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+stage_songs_to_redshift
